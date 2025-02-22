bebcec53364a912259365f2cdbf039317089076d
JahanaviChaturvedi <118982130+JahanaviChaturvedi@users.noreply.github.com>
2025-02-22 10:22:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 //your JS code here. If required.
-let student = {
-	name:"Ram"
+let student ={
+	name:"Ram",
+	age:"22",
+	city:"Ayodhya"
+};
+
+let student2 ={
+	name:"Shyam"
 }
 
 function getKeys(obj){
 	return Object.keys(obj);
 }
 console.log(getKeys(student));
+console.log(getKeys(student2));
